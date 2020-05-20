cdbf0dd893a01fbe4b8e357d5e79391f7dbd13f1
Oliver Goldschmidt <o.goldschmidt@tuhh.de>
2020-05-20 10:27:04 +0200

    layout changes

--- themes/bootprint3-tub/theme.config.php
+++ themes/bootprint3-tub/theme.config.php
@@ -1,4 +1,12 @@
 <?php
 return [
-    'extends' => 'bootstrap3-tub'
+    'extends' => 'bootstrap3-tub',
+    'css' => [
+        'tub.css',
+        'style.css'
+    ],
+    'less' => [
+        'active' => true,
+        'compiled.less'
+    ],
 ];
